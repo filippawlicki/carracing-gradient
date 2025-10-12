fd62ce8bc56d59579db81fa7ef85fa15dcfe0905
adam <adam.klamrowski.ak@gmail.com>
2025-10-12 18:24:33 +0200

    chore: deleted unnecessary variable

--- multi_car_racing/GameConfig.py
+++ multi_car_racing/GameConfig.py
@@ -7,7 +7,6 @@ class GameConfig:
     width: int = 1600
     height: int = 1000
     fps: int = 60
-    round_time: int = 1000  # milliseconds
     minimap_size: int = 180
     seed: Optional[int] = None
     number_of_cars: int = 2
